4804fb5e9e2858fbf7f03ac0d75ce21d8b1c0e42
Whoistolu <digittolu25@gmail.com>
2021-12-16 19:56:27 +0100

    Fix linter errors

--- clasroom.rb
+++ clasroom.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require_releative './student'
 
 class Classroom
--- rental.rb
+++ rental.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 class Rental
   attr_accessor :date
 
-  def initialize(book, person)
+  def initialize(book, person, date = DateTime.now())
     @date = date
     self.book = book
     self.person = person
