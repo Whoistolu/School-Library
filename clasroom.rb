13857a6adf9b6feacd448e461e3d57b7660cbc66
Whoistolu <digittolu25@gmail.com>
2021-12-16 18:33:40 +0100

    Create Classroom class, create has-many, belong-to relationships between classroom and student

---
+++ clasroom.rb
@@ -0,0 +1,15 @@
+require_releative './student'
+
+class Classroom
+  attr_accessor :label, :students
+
+  def initialize(label)
+    @label = label
+    @students = []
+  end
+
+  def add_student(student)
+    @students.push(student) unless @students.include?(student)
+    student.classroom = self
+  end
+end
